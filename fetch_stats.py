96716cf9f9f9f0e1554067c7cc18e4bb057d4571
Aimee Co <aimeekco@gmail.com>
2026-02-02 20:42:13 -0800

    clean up requirements and refactor fetch_stats to stats_utils

--- fetch_stats.py
+++ fetch_stats.py
@@ -1,14 +1,18 @@
 import requests
 import json
+import os
 
 # URL for the stats endpoint
-url = "https://www.nytimes.com/svc/crosswords/v3/221431134/stats-and-streaks.json?date_start=1988-01-01&start_on_monday=true"
+user_id = os.environ.get("NYT_USER_ID", "221431134")
+url = f"https://www.nytimes.com/svc/crosswords/v3/{user_id}/stats-and-streaks.json?date_start=1988-01-01&start_on_monday=true"
 
 # header with your cookies for authentication
 headers = {
     "User-Agent": "Mozilla/5.0",
-    # "Cookie": "nyt-a=Eu9gF_ec8lI6xbSqZwXPOa; nyt-gdpr=0; nyt-purr=cfshcfhssckfsdfhhgah2; purr-cache=<G_<C_<T0<Tp1_<Tp2_<Tp3_<Tp4_<Tp7_<a0_<K0<S0<r<ur; purr-pref-agent=<G_<C_<T0<Tp1_<Tp2_<Tp3_<Tp4_<Tp7_<a12; datadome=rgKIM4JwpJ4FevO_Iq4MretPVtOLNMSauvD2xIdonfDT0ZWNamZUG1q_GUWBwQ33NZGWYMZyDuAZmToFi_LVAt28TevNjPBA_vLQ85SN9LN3nMDCv_YbC0lAU6OSM_Qh;"
 }
+
+if "NYT_COOKIE" in os.environ:
+    headers["Cookie"] = os.environ["NYT_COOKIE"]
 
 def fetch_stats():
     # fetch stats from api
@@ -22,6 +26,7 @@ def fetch_stats():
         print("Stats fetched and saved successfully!")
     else:
         print(f"Failed to fetch stats. Status code: {response.status_code}")
+        raise Exception("Failed to fetch stats")
 
 if __name__ == "__main__":
     fetch_stats()
